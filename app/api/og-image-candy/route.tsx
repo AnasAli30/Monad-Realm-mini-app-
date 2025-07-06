cf701427b3e3fda1ec0f980a35e5053a9d140f2e
AnasAli30 <anasali12665@gmail.com>
2025-07-06 18:18:20 +0530

    change the generated image

--- app/api/og-image-candy/route.tsx
+++ app/api/og-image-candy/route.tsx
@@ -58,7 +58,7 @@ export async function GET(request: NextRequest) {
               width: '100%',
               height: '100%',
               objectFit: 'contain',
-              objectPosition: 'center',
+              objectPosition: 'top center',
             }}
           />
 
